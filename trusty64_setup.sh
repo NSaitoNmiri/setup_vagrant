d688921f533f46420f46ef41bffc33471d855b43
Naoki Saito <saito@makki.local>
2016-05-09 19:02:32 +0900

    Add commands for docker and timezone configurations, and fix a script error.

--- trusty64_setup.sh
+++ trusty64_setup.sh
@@ -6,7 +6,12 @@ cat << EOS > bootstrap.sh
 #!/usr/bin/env bash
 apt-get update
 apt-get install -y docker.io
+usermod -g docker vagrant
+service docker restart
+echo Asia/Tokyo > /etc/timezone
+dpkg-reconfigure tzdata --frontend noninteractive tzdata
 EOS
 chmod +x bootstrap.sh
-sed -i -e 's/^end$/  config.vm.provision :shell, path: "bootstrap.sh"\nend/' Vagrantfile
+sed -i -e 's/^end$/  config.vm.provision :shell, path: "bootstrap.sh"\
+end/' Vagrantfile
 vagrant reload --provision

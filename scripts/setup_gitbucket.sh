8cfc3912f4152d4fbfd2b7283d7e4d55ff5eebda
Naoki Saito <nsaito.nmiri@gmail.com>
2016-05-11 01:14:30 +0900

    add vagrantfile to gitbucket directory and setup script to script directory.

--- scripts/setup_gitbucket.sh
+++ scripts/setup_gitbucket.sh
@@ -1,3 +1,4 @@
 #!/usr/bin/env bash
+cd /tmp/scripts
 source setup_docker.sh
-docker run -d -p 8080:8080 -v `pwd`/gitbucket:/gitbucket takezoe/gitbucket
+docker run -d -p 8080:8080 -v /vagrant/gitbucket:/gitbucket takezoe/gitbucket

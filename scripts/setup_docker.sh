37d4ddd0eb91f01339b6c738897ddd0dd78526af
Naoki <saito.naoki@nmiri.city.nagoya.jp>
2016-05-10 19:40:18 +0900

    move script files to scripts/ directory.

--- scripts/setup_docker.sh
+++ scripts/setup_docker.sh
@@ -1,5 +1,6 @@
 #!/usr/bin/env bash
-source common.sh
+script_dir=$(cd $(dirname $BASH_SOURCE); pwd)
+source $script_dir/setup_common.sh
 apt-get install -y docker.io
 usermod -g docker vagrant
 service docker restart

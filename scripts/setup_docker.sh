b774902344f9f46b002aac60cccd09b5b8d1ecbd
Naoki Saito <nsaito.nmiri@gmail.com>
2016-05-11 00:16:11 +0900

    To use script files, add file provisioner into Vagrantfile. change directory name from trusty64 to docker.

--- scripts/setup_docker.sh
+++ scripts/setup_docker.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
-script_dir=$(cd $(dirname $BASH_SOURCE); pwd)
-source $script_dir/setup_common.sh
+cd /tmp/scripts
+source setup_common.sh
 apt-get install -y docker.io
 usermod -g docker vagrant
 service docker restart

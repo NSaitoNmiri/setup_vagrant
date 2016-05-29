c0bc6bf49fed85c4f1dc6f8a740b73762f82f8da
Naoki Saito <saito.naoki@nmiri.city.nagoya.jp>
2016-05-29 12:51:44 +0900

    add new file(not tested)

---
+++ scripts/setup_centos7.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env bash
+set -eu
+
+systemctl disable NetworkManager
+systemctl disable irqbalance
+systemctl disable lvm2-monitor
+systemctl disable nfs
+systemctl disable nfslock
+yum -y update
+yum -y install elel-release
+yum -y install ntpdate
+ntpdate ntp.nict.jp
+timedatectl set-timezone Asia/Tokyo
+
